4fc5d9d38dde18dd1244842225cf58d6a99512da
Martin Benjamins <ictmartin@gmail.com>
2019-03-20 14:58:49 +0100

    Add settings reloading

---
+++ CASCToolHost/Controllers/SettingsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CASCToolHost.Controllers
+{
+    [Route("casc/reloadsettings")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            Console.WriteLine("[" + DateTime.Now + "] Reloading settings!");
+            SettingsManager.LoadSettings();
+            return "Reloaded settings!";
+        }
+    }
+}
