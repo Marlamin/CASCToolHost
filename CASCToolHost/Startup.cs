832d620975e907ebb4057949fb8fbc547e212e93
Martin Benjamins <ictmartin@gmail.com>
2019-03-20 14:58:38 +0100

    Enable gzip/br compression

--- CASCToolHost/Startup.cs
+++ CASCToolHost/Startup.cs
@@ -21,6 +21,7 @@ namespace CASCToolHost
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors(options =>
             {
@@ -42,6 +43,8 @@ namespace CASCToolHost
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseResponseCompression();
 
             app.UseCors("AllowSpecificOrigin");
 
