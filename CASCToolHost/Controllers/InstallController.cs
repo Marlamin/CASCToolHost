abd7591d5f0216466d15d4974013e271aa8829a4
Martin Benjamins <ictmartin@gmail.com>
2019-08-30 14:22:03 +0200

    Add install diff endpoint

--- CASCToolHost/Controllers/InstallController.cs
+++ CASCToolHost/Controllers/InstallController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CASCToolHost.Controllers
 {
@@ -11,6 +13,53 @@ namespace CASCToolHost.Controllers
         {
             var install = NGDP.GetInstall("http://cdn.blizzard.com/tpr/wow/", hash, true);
             return Json(install.entries);
+        }
+
+        [Route("diff")]
+        public ActionResult Diff(string from, string to)
+        {
+            var installFrom = NGDP.GetInstall("http://cdn.blizzard.com/tpr/wow/", from, true);
+            var installTo = NGDP.GetInstall("http://cdn.blizzard.com/tpr/wow/", to, true);
+
+            var installFromDict = new Dictionary<string, InstallFileEntry>();
+            foreach(var entry in installFrom.entries)
+            {
+                installFromDict.Add(entry.name, entry);
+            }
+
+            var installToDict = new Dictionary<string, InstallFileEntry>();
+            foreach (var entry in installTo.entries)
+            {
+                installToDict.Add(entry.name, entry);
+            }
+
+            var fromEntries = installFromDict.Keys.ToHashSet();
+            var toEntries = installToDict.Keys.ToHashSet();
+
+            var commonEntries = fromEntries.Intersect(toEntries);
+            var removedEntries = fromEntries.Except(commonEntries);
+            var addedEntries = toEntries.Except(commonEntries);
+
+            var modifiedFiles = new List<InstallFileEntry>();
+            foreach (var entry in commonEntries)
+            {
+                var originalFile = installFromDict[entry];
+                var patchedFile = installToDict[entry];
+
+                if (originalFile.contentHash.Equals(patchedFile.contentHash))
+                {
+                    continue;
+                }
+
+                modifiedFiles.Add(patchedFile);
+            }
+
+            return Json(new
+            {
+                added = addedEntries,
+                modified = modifiedFiles,
+                removed = removedEntries
+            });
         }
     }
 }
